8d82cd6ce439b84a8334a7ca7e8dedfd990ae29a
user10 <user10@192.168.1.33>
2018-06-06 11:49:25 +0900

    tomcatのバージョンを7に

--- src/dao/WorkDAO.java
+++ src/dao/WorkDAO.java
@@ -1,5 +1,20 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.naming.NamingException;
+
+import model.WorkTimeModel;
+
 public class WorkDAO {
 
+	public void insertWorkTime(WorkTimeModel wt) throws NamingException, SQLException{
+		Connection conn=null;
+		PreparedStatement pstmt=null;
+
+		conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/gameinfo?verifyServerCertificate=false&useSSL=false&requireSSL=false","root","password");
+	}
 }
--- src/others/LoginLogic.java
+++ src/others/LoginLogic.java
@@ -36,4 +36,7 @@ public class LoginLogic {
 
 		return password;
 	}
+
+
+
 }
