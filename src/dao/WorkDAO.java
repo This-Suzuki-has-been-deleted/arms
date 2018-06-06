ed9ebbe2c362b4d5f1cb5d8b6d13377d9655b752
user10 <user10@192.168.1.33>
2018-06-06 14:40:13 +0900

    WorkDAO完成

--- src/dao/WorkDAO.java
+++ src/dao/WorkDAO.java
@@ -3,7 +3,10 @@ package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.naming.NamingException;
 
@@ -11,10 +14,148 @@ import model.WorkTimeModel;
 
 public class WorkDAO {
 
-	public void insertWorkTime(WorkTimeModel wt) throws NamingException, SQLException{
-		Connection conn=null;
-		PreparedStatement pstmt=null;
+	// 打刻するメソッド
+	public void insertWorkTime(WorkTimeModel wt) throws NamingException,
+			SQLException, InstantiationException, IllegalAccessException,
+			ClassNotFoundException {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
 
-		conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/gameinfo?verifyServerCertificate=false&useSSL=false&requireSSL=false","root","password");
+		// データベース接続
+		conn = DriverManager
+				.getConnection(
+						"jdbc:mysql://localhost:3306/gameinfo?verifyServerCertificate=false&useSSL=false&requireSSL=false",
+						"root", "password");
+
+		// クラスのインスタンスを取得
+		Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+		// 自動コミットをオフ
+		conn.setAutoCommit(false);
+		String sql = "insert into employeeworktime(employeeno,year,month,day,attendance) values(?,?,?,?,?)";
+		pstmt = conn.prepareStatement(sql);
+
+		pstmt.setString(1, wt.getEmployeeNo());
+		pstmt.setInt(2, wt.getYear());
+		pstmt.setInt(3, wt.getMonth());
+		pstmt.setInt(4, wt.getDay());
+		pstmt.setTime(5, wt.getAttendance());
+
+		if (pstmt.executeUpdate() > 0) {
+			conn.commit();
+		} else {
+			conn.rollback();
+		}
+
+		pstmt.close();
+		conn.close();
+	}
+
+	public void updateWorkTime(WorkTimeModel wt) throws SQLException,
+			InstantiationException, IllegalAccessException,
+			ClassNotFoundException {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+
+		// データベース接続
+		conn = DriverManager
+				.getConnection(
+						"jdbc:mysql://localhost:3306/gameinfo?verifyServerCertificate=false&useSSL=false&requireSSL=false",
+						"root", "password");
+
+		// クラスのインスタンスを取得
+		Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+		// 自動コミットをオフ
+		conn.setAutoCommit(false);
+
+		String sql = "update employeeworktime set worktimeflg=?,attendance=?,leaving=? where employeeno=?";
+		pstmt = conn.prepareStatement(sql);
+		pstmt.setBoolean(1, wt.isWorkTimeFlg());
+		pstmt.setTime(2, wt.getAttendance());
+		pstmt.setTime(3, wt.getLeaving());
+		pstmt.setString(4, wt.getEmployeeNo());
+
+		if (pstmt.executeUpdate() > 0) {
+			conn.commit();
+		} else {
+			conn.rollback();
+		}
+
+		pstmt.close();
+		conn.close();
+	}
+
+	public WorkTimeModel findWorkTime(String eno, int y, int m, int d)
+			throws SQLException, InstantiationException,
+			IllegalAccessException, ClassNotFoundException {
+		WorkTimeModel wt = new WorkTimeModel();
+
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+
+		// データベース接続
+		conn = DriverManager
+				.getConnection(
+						"jdbc:mysql://localhost:3306/gameinfo?verifyServerCertificate=false&useSSL=false&requireSSL=false",
+						"root", "password");
+
+		// クラスのインスタンスを取得
+		Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+		// 自動コミットをオフ
+		conn.setAutoCommit(false);
+		String sql = "select * from employeeworktime where employeeno=? and year=?,month=?,day=?";
+		pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, eno);
+		pstmt.setInt(2, y);
+		pstmt.setInt(3, m);
+		pstmt.setInt(4, d);
+		ResultSet rs = pstmt.executeQuery();
+
+		wt.setEmployeeNo(rs.getString("employeeno"));
+		wt.setYear(rs.getInt("year"));
+		wt.setMonth(rs.getInt("month"));
+		wt.setDay(rs.getInt("day"));
+		wt.setWorkTimeFlg(rs.getBoolean("worktimeflg"));
+		wt.setAttendance(rs.getTime("attendance"));
+		wt.setLeaving(rs.getTime("leaving"));
+
+		return wt;
+	}
+
+	public List<WorkTimeModel> d_findByEmployeeNo(String eno)
+			throws SQLException, InstantiationException,
+			IllegalAccessException, ClassNotFoundException {
+		List<WorkTimeModel> list = new ArrayList<WorkTimeModel>();
+
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		// データベース接続
+		conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/gameinfo?verifyServerCertificate=false&useSSL=false&requireSSL=false","root", "password");
+
+		// クラスのインスタンスを取得
+		Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+		// 自動コミットをオフ
+		conn.setAutoCommit(false);
+		String sql = "select * from employeeworktime where employeeno=?";
+		pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, eno);
+
+		ResultSet rs = pstmt.executeQuery();
+		while(rs.next()){
+			WorkTimeModel wt = new WorkTimeModel();
+			wt.setEmployeeNo(rs.getString("employeeno"));
+			wt.setYear(rs.getInt("year"));
+			wt.setMonth(rs.getInt("month"));
+			wt.setDay(rs.getInt("day"));
+			wt.setWorkTimeFlg(rs.getBoolean("worktimeflg"));
+			wt.setAttendance(rs.getTime("attendance"));
+			wt.setLeaving(rs.getTime("leaving"));
+			list.add(wt);
+		}
+
+		return list;
 	}
 }
