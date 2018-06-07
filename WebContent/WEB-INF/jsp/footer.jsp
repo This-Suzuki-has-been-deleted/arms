50f820ea4cef470757932c4ca37a0aa329184f90
user10 <user10@192.168.1.33>
2018-06-07 15:35:35 +0900

    クラスパス変更

--- WebContent/WEB-INF/jsp/footer.jsp
+++ WebContent/WEB-INF/jsp/footer.jsp
@@ -1,3 +1,3 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<div class="footer">Copyright🄫 2018 Ozaworld inc/ All Rights Reserved.</div>
+<div class="footer"><footer>Copyright🄫 2018 Ozaworld inc/ All Rights Reserved.</footer></div>
--- src/servlet/TimeStampServlet.java
+++ src/servlet/TimeStampServlet.java
@@ -1,17 +1,21 @@
 package servlet;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class TimeStampServlet
  */
 public class TimeStampServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -24,14 +28,19 @@ public class TimeStampServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		String msg="ログインを行ってください。";
+		session.setAttribute("String", msg);
+		RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+		rd.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		LocalDateTime date = LocalDateTime.now();
 	}
 
 }
