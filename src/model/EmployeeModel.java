6609391211d70bd1ed4b01a5abe9d59ea21aa814
user10 <user10@USER-PC>
2018-06-07 11:23:27 +0900

    model最新

--- src/model/EmployeeModel.java
+++ src/model/EmployeeModel.java
@@ -2,29 +2,17 @@ package model;
 
 public class EmployeeModel {
 	private String employeeNo;
-	private String depNo;
-	private String authNo;
 	private String employeeName;
 	private String password;
 	private int delFlg;
+	private AuthModel authModel = new AuthModel();
+	private DepModel depModel = new DepModel();
 
 	public String getEmployeeNo() {
 		return employeeNo;
 	}
 	public void setEmployeeNo(String employeeNo) {
 		this.employeeNo = employeeNo;
-	}
-	public String getDepNo() {
-		return depNo;
-	}
-	public void setDepNo(String depNo) {
-		this.depNo = depNo;
-	}
-	public String getAuthNo() {
-		return authNo;
-	}
-	public void setAuthNo(String authNo) {
-		this.authNo = authNo;
 	}
 	public String getEmployeeName() {
 		return employeeName;
@@ -44,6 +32,40 @@ public class EmployeeModel {
 	public void setDelFlg(int delFlg) {
 		this.delFlg = delFlg;
 	}
-
-
+	public AuthModel getAuthModel() {
+		return authModel;
+	}
+	public void setAuthModel(AuthModel authModel) {
+		this.authModel = authModel;
+	}
+	public DepModel getDepModel() {
+		return depModel;
+	}
+	public void setDepModel(DepModel depModel) {
+		this.depModel = depModel;
+	}
+	public String getAuthNo() {
+		return authModel.getAuthNo();
+	}
+	public void setAuthNo(String authNo) {
+		authModel.setAuthNo(authNo);
+	}
+	public String getAuthName() {
+		return authModel.getAuthName();
+	}
+	public void setAuthName(String authName) {
+		authModel.setAuthName(authName);
+	}
+	public String getDepNo() {
+		return depModel.getDepNo();
+	}
+	public void setDepNo(String depNo) {
+		depModel.setDepNo(depNo);
+	}
+	public String getDepName() {
+		return depModel.getDepName();
+	}
+	public void setDepName(String depName) {
+		depModel.setDepName(depName);
+	}
 }
