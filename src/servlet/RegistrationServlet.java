bc172a6ab83cfdfafcc273b29e1b92cc9c4f9d70
user10 <user10@USER-PC>
2018-06-07 16:26:50 +0900

    登録途中

--- src/servlet/RegistrationServlet.java
+++ src/servlet/RegistrationServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import model.EmployeeModel;
 import others.LoginLogic;
+import dao.EmployeeDAO;
 
 /**
  * Servlet implementation class RegistrationServlet
@@ -40,37 +42,42 @@ public class RegistrationServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+
 		EmployeeModel employeeModel = new EmployeeModel();
+		EmployeeModel myEmp = (EmployeeModel) request.getAttribute("Employee");
+		
 		LoginLogic ll = new LoginLogic();
+		EmployeeDAO ed = new EmployeeDAO();
 
 		String textCode;
 		String textName;
 		String selectDivisionNo;
 		String selectAuthorityNo;
 		String pageFlg = "rs";
-
+		String msg = null;
 		boolean flg = false;
 
-		String msg= null;
-		try{
-			msg = null;
+		//部署 division,dep
+		//権限 authority
 
+		try{
 			textCode = request.getParameter("textCode");
 			textName = request.getParameter("textName");
-			selectDivisionNo = request.getParameter("selectDivisionNo");
-			selectAuthorityNo = request.getParameter("selectAuthorityNo");
 
-			if(selectDivisionNo == null ){
-
+			if(myEmp.getAuthNo() == "003" || myEmp.getAuthNo() == "999"){
+				selectDivisionNo = request.getParameter("selectDivisionNo");
+				selectAuthorityNo = request.getParameter("selectAuthorityNo");
 			}else{
-				selectDivisionNo = "01";
+				selectDivisionNo = myEmp.getDepNo();
 				selectAuthorityNo = "001";
 			}
+
 			/**
 			 * 入力チェック
 			 */
+
 			if(flg){
-				msg = "入力に誤りがあります。";
+				msg = "入力形式に誤りがあります。";
 				request.setAttribute("msg",msg);
 			}
 
@@ -81,19 +88,22 @@ public class RegistrationServlet extends HttpServlet {
 			employeeModel.setPassword(ll.passHash("pass1234"));
 			employeeModel.setDelFlg(1);
 		}catch(NullPointerException e){
-			msg = msg + "\n入力されていない項目があります。";
+			msg = msg + "\n未入力項目があります。";
 		}
 
+		ArrayList<EmployeeModel> employeelist = ed.findAll();
 
-
-
-
+		for(EmployeeModel em:employeelist){
+			if(employeeModel.getEmployeeNo().equals(em.getEmployeeNo())){
+				msg = msg + "\n社員番号が重複しています。";
+			}
+		}
 		/**
 		 * 重複チェック
 		 */
 
 		request.setAttribute("employeeModel",employeeModel);
-		request.setAttribute("flg",flg);
+		request.setAttribute("pageFlg",pageFlg);
 
 		RequestDispatcher dispatcher = request
 				.getRequestDispatcher("/conf.jsp");
