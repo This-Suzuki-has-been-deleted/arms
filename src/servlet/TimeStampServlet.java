347abc753634b5519ff66ffbb693e9cf80fda762
user10 <user10@192.168.1.33>
2018-06-11 14:28:09 +0900

    アノテーション付け忘れ

--- src/servlet/TimeStampServlet.java
+++ src/servlet/TimeStampServlet.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,6 +22,7 @@ import dao.WorkDAO;
 /**
  * Servlet implementation class TimeStampServlet
  */
+@WebServlet("/index")
 public class TimeStampServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
