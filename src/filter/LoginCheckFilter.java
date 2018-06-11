283b42853673dabc83c5667f4414706c679b268b
user10 <user10@192.168.1.33>
2018-06-11 11:24:53 +0900

    フィルター完成

--- src/filter/LoginCheckFilter.java
+++ src/filter/LoginCheckFilter.java
@@ -1,5 +1,64 @@
 package filter;
 
-public class LoginCheckFilter {
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.EmployeeModel;
+
+/**
+ * Servlet Filter implementation class LoginCheckFilter
+ */
+@WebFilter(urlPatterns={"/TimeStampServlet","/ConfServlet","/EmployeeServlet","/RegistrationServlet","/WorkServlet","/ChangeServlet"})
+public class LoginCheckFilter implements Filter {
+
+    /**
+     * Default constructor.
+     */
+    public LoginCheckFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest httpRequest = (HttpServletRequest)request;
+		HttpServletResponse httpResponse = (HttpServletResponse)response;
+		HttpSession session = httpRequest.getSession();
+
+		EmployeeModel em = (EmployeeModel)session.getAttribute("employee");
+		if(em != null){
+			chain.doFilter(request, response);
+		}else{
+			session.setAttribute("msg", "ログインしてください。");
+			RequestDispatcher dr = request.getRequestDispatcher("login.jsp");
+			dr.forward(httpRequest, httpResponse);
+		}
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
 
 }
