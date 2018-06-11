71c260ab9b1becbc10a9818274ee5c087b9ca23c
user10 <user10@192.168.1.33>
2018-06-11 15:21:51 +0900

    施行

--- src/servlet/TimeStampServlet.java
+++ src/servlet/TimeStampServlet.java
@@ -6,7 +6,6 @@ import java.time.LocalDateTime;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -22,7 +21,7 @@ import dao.WorkDAO;
 /**
  * Servlet implementation class TimeStampServlet
  */
-@WebServlet("/index")
+
 public class TimeStampServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
